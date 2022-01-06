c969cb5bc746f648acea6ba88f682030bdf09731
JuliaJia <juliajia25191@gmail.com>
2022-01-06 21:56:31 +0800

    add Authentication function

--- Twitter/settings.py
+++ Twitter/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'accounts',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
