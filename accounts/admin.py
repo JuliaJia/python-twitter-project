2e77378b18f83d0bba171651d22c6e2c63590c2f
JuliaJia <juliajia25191@gmail.com>
2022-01-13 07:00:41 +0800

    add ORM learn

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from accounts.models import Tweet_user
 
 # Register your models here.
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('user','content','created_at','likes_count','comments_count')
+
+admin.site.register(Tweet_user,TweetAdmin)
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_id = models.TextField(default=0)
+
+class Photo(models.Model):
+    Photo_id = models.TextField(default=0)
+
+
+class Tweet(models.Model):
+    user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    photos = models.ManyToManyField(Photo,blank=True)
+    likes_count = models.BigIntegerField(default=0,null=True)
+    comments_count = models.BigIntegerField(default=0,null=True)
+
+
+
+class Tweet_user(models.Model):
+    user = models.CharField(max_length=200,null=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes_count = models.BigIntegerField(default=0,null=True)
+    comments_count = models.BigIntegerField(default=0,null=True)
+
